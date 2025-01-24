04231cd0fbfbea9fcd115c463f799134745ad377
subhanshu jha <subhanshujha001@gmail.com>
2025-01-24 20:37:47 +0530

    deployment chnages

--- src/app/(dashboard)/builder/[id]/error.tsx
+++ src/app/(dashboard)/builder/[id]/error.tsx
@@ -1,11 +1,12 @@
 "use client"
 import { Button } from '@/components/ui/button'
 import React from 'react'
+import Link from 'next/link'
 const error = () => {
   return (
     <div className='flex flex-col gap-4 items-center justify-center h-full w-full'> 
     <h2 className='text-3xl font-bold'>Page Doesnt exists</h2> 
-    <a href="/"><Button variant={"link"}>Go Back To Home Page</Button></a>
+    <Link href={"/"}><Button variant={"link"}>Go Back To Home Page</Button></Link>
     </div>
   )
 }
