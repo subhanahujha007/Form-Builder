159e0ea0c06a4c17024bd61d5922e168c6892883
subhanshu jha <subhanshujha001@gmail.com>
2025-01-24 21:24:56 +0530

    deployment chnages

--- src/app/(dashboard)/builder/[id]/page.tsx
+++ src/app/(dashboard)/builder/[id]/page.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { GetFormByID } from '@/actions/form';
 import FormBuilder from '@/components/FormBuilder';
 
+// @ts-ignore
 async function page({ params }: { params: Promise<{ id: string }> }) {
-  const  id  = (await params).id;  
+  const id = (await params).id;  
   const form = await GetFormByID(Number(id));
 
   if (!form) throw new Error('Form not found');
